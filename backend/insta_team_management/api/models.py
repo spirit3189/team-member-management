4c1cbbc4c6f1e928ab751831e8fc836882c8f84e
JagdeeshK <Jagdeesh.net@gmail.com>
2024-12-09 22:32:22 -0800

    fix(): Add phone number validation, edit init fixture data

--- backend/insta_team_management/api/models.py
+++ backend/insta_team_management/api/models.py
@@ -1,15 +1,20 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 class TeamMember(models.Model):
     ROLE_CHOICES = [
         ('regular', 'Regular - Can’t delete members'),
         ('admin', 'Admin - Can delete members'),
     ]
+    phone_regex = RegexValidator(
+        regex=r'^\d{3}-\d{3}-\d{4}$',
+        message="Phone number must have the format xxx-xxx-xxxx"
+    )
 
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20)
+    phone = models.CharField(validators=[phone_regex], max_length=12, blank=False)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
     def __str__(self):
--- client/src/components/TeamMemberForm.jsx
+++ client/src/components/TeamMemberForm.jsx
@@ -2,28 +2,64 @@ import React, { useState } from 'react';
 import styles from './TeamMemberForm.module.css';
 
 const TeamMemberForm = ({ memberData, onSubmit, onDelete, showDeleteButton = false }) => {
+    const PHONE_ERROR_MESSAGE = 'Phone number must have the format xxx-xxx-xxxx';
     const initialData = {
         first_name: '',
         last_name: '',
         email: '',
         phone: '',
         role: 'regular',
-    }
+    };
     const [formData, setFormData] = useState(memberData || initialData);
+    const [error, setError] = useState('');
 
-    const handleChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handlePhoneChange = (e) => {
+        const inputValue = e.target.value;
+        const formattedValue = formatPhoneNumber(inputValue);
+        setFormData({ ...formData, phone: formattedValue });
+        if (isValidPhoneNumber(formattedValue)) {
+            setError('');
+        } else {
+            setError(PHONE_ERROR_MESSAGE);
+        }
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        if (!isValidPhoneNumber(formData.phone)) {
+            setError(PHONE_ERROR_MESSAGE);
+            return;
+        }
         onSubmit(formData);
     };
 
     const handleDelete = (e) => {
         e.preventDefault();
-        const confirmDelete = window.confirm("Are you sure you want to delete this team member?");
+        const confirmDelete = window.confirm('Are you sure you want to delete this team member?');
         if (confirmDelete) {
             onDelete(formData.id);
         }
+    };
+
+    const formatPhoneNumber = (value) => {
+        const formattedPhoneNumber = value.replace(/\D/g, '');
+
+        if (formattedPhoneNumber.length <= 3) {
+            return formattedPhoneNumber;
+        } else if (formattedPhoneNumber.length <= 6) {
+            return `${formattedPhoneNumber.slice(0, 3)}-${formattedPhoneNumber.slice(3)}`;
+        } else {
+            return `${formattedPhoneNumber.slice(0, 3)}-${formattedPhoneNumber.slice(3, 6)}-${formattedPhoneNumber.slice(6, 10)}`;
+        }
+    };
+
+    const isValidPhoneNumber = (phone) => {
+        const phoneRegex = /^\d{3}-\d{3}-\d{4}$/;
+        return phoneRegex.test(phone);
     };
 
     return (
@@ -54,14 +90,18 @@ const TeamMemberForm = ({ memberData, onSubmit, onDelete, showDeleteButton = fal
                     type="email"
                     required
                 />
-                <input
-                    name="phone"
-                    value={formData.phone}
-                    onChange={handleChange}
-                    placeholder="Phone"
-                    type="tel"
-                    required
-                />
+                <div>
+                    <input
+                        id="phone"
+                        type="tel"
+                        name="phone"
+                        value={formData.phone}
+                        onChange={handlePhoneChange}
+                        placeholder="Phone"
+                        required
+                    />
+                    {error && <p style={{ color: 'red' }}>{error}</p>}
+                </div>
             </div>
 
             <div>
@@ -96,9 +136,17 @@ const TeamMemberForm = ({ memberData, onSubmit, onDelete, showDeleteButton = fal
 
             <div className={styles.buttonContainer}>
                 {showDeleteButton && (
-                    <button className={`${styles.ctaButtons} ${styles.deleteButton}`} onClick={handleDelete}>Delete</button>)
-                }
-                <button type="submit" className={`${styles.ctaButtons} ${styles.saveButton}`}>Save</button>
+                    <button
+                        type="button"
+                        className={`${styles.ctaButtons} ${styles.deleteButton}`}
+                        onClick={handleDelete}
+                    >
+                        Delete
+                    </button>
+                )}
+                <button type="submit" className={`${styles.ctaButtons} ${styles.saveButton}`}>
+                    Save
+                </button>
             </div>
         </form>
     );
